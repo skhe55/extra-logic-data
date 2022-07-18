54f5787b48e72376eca6a0603bd8e2f45352de91
skhe55 <flashback2019@mail.ru>
2022-07-18 16:20:11 +0700

    heroku url

--- app/config/config.js
+++ app/config/config.js
@@ -2,10 +2,10 @@ const fs = require('fs');
 
 module.exports = {
   development: {
-    username: 'lodedbxqcocrmr',//'postgres',
-    password: 'ca83e56dc7a25bbfc2ab5cc7d98de985bb5dff29342f36f5cf44b56176221c6a',//'root',
-    database: 'd2qa076nlisvei', //'FormData',
-    host: 'ec2-3-219-229-143.compute-1.amazonaws.com', //"form_db",
+    username: 'jktxjdclrdyavs',//'postgres',
+    password: 'fb69f06a8cead36d6e88ec1ff6f92dc9cc563a61df4816be1df31b49c76a2da9',//'root',
+    database: 'dcmier78m60b0o', //'FormData',
+    host: 'ec2-54-161-255-125.compute-1.amazonaws.com', //"form_db",
     port: 5432,
     dialect: 'postgres',
   }
--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  HOST: 'ec2-3-219-229-143.compute-1.amazonaws.com', // process.env.NODE_ENV === 'docker' ? "form_db" : "localhost",
-  USER: 'lodedbxqcocrmr', //"postgres",
-  PASSWORD: 'ca83e56dc7a25bbfc2ab5cc7d98de985bb5dff29342f36f5cf44b56176221c6a',//"root",
-  DB: 'd2qa076nlisvei', //"FormData",
+  HOST: "ec2-54-161-255-125.compute-1.amazonaws.com", // process.env.NODE_ENV === 'docker' ? "form_db" : "localhost",
+  USER: 'jktxjdclrdyavs', //"postgres",
+  PASSWORD: 'fb69f06a8cead36d6e88ec1ff6f92dc9cc563a61df4816be1df31b49c76a2da9',//"root",
+  DB: 'dcmier78m60b0o', //"FormData",
   dialect: "postgres",
 };
 
